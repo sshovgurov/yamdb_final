3ca8444987fd333322e071279db9fb9f479de7ea
sshovgurov <rdigga99@gmail.com>
2021-11-15 00:35:39 +0300

    rgweg

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 load_dotenv()
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = 'p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs'
 
 DEBUG = False
 
