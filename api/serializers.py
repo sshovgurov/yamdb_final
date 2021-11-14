43b77c6038f7dd7cabe99833b6b89524054559a8
sshovgurov <rdigga99@gmail.com>
2021-11-14 19:33:27 +0300

    34

--- api/serializers.py
+++ api/serializers.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404
 from django.utils import timezone
 from rest_framework import serializers
+
 from reviews.models import Category, Comment, Genre, Review, Title, User
 
 
--- api/views.py
+++ api/views.py
@@ -3,12 +3,14 @@ from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import send_mail
 from django.db.models import Avg
 from django.shortcuts import get_object_or_404
+
 from rest_framework import filters, status, viewsets, serializers
 from rest_framework.decorators import action, api_view, permission_classes
 from rest_framework.permissions import (AllowAny, IsAuthenticated,
                                         IsAuthenticatedOrReadOnly)
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import AccessToken
+
 from reviews.models import Category, Comment, Genre, Review, Title, User
 
 from .permissions import (IsAdmin, IsAdminOrReadOnly, IsAuthor, IsModerator,
