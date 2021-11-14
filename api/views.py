edc5bf2a693267d1a12ba04f31f82ab41fbdc132
sshovgurov <rdigga99@gmail.com>
2021-11-14 19:43:11 +0300

    45

--- api/views.py
+++ api/views.py
@@ -3,7 +3,6 @@ from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import send_mail
 from django.db.models import Avg
 from django.shortcuts import get_object_or_404
-
 from rest_framework import filters, serializers, status, viewsets
 from rest_framework.decorators import action, api_view, permission_classes
 from rest_framework.permissions import (AllowAny, IsAuthenticated,
